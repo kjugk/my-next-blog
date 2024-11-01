604814d4b2449e40de31f179cbc5256bae9db60b
UegakiKoji <kjugkjk@gmail.com>
2024-11-01 12:04:25 +0900

    feature: Add new post page

---
+++ src/app/post/new/page.tsx
@@ -0,0 +1,11 @@
+import { PostEditor } from "@/features/post/editor/PostEditor";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>New Post</h1>
+
+      <PostEditor />
+    </div>
+  );
+}
---
+++ src/features/post/editor/PostEditor.tsx
@@ -0,0 +1,10 @@
+export const PostEditor = () => {
+  return (
+    <div>
+      <div>
+        <textarea></textarea>
+      </div>
+      <div>display html</div>
+    </div>
+  );
+};
