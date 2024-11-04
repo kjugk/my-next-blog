9407a9ce7911ef41dc4e884574818a039e7e5b93
UegakiKoji <kjugkjk@gmail.com>
2024-11-04 22:19:59 +0900

    コメント追加

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
+// refs: https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices
 const prismaClientSingleton = () => {
   return new PrismaClient();
 };
