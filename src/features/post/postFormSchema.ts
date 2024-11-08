1d9fe34c3dad8e131dabd0ea56363915c904dbc1
UegakiKoji <kjugkjk@gmail.com>
2024-11-08 22:11:40 +0900

    fix: add form validation

---
+++ src/features/post/postFormSchema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+const required = () => z.string().min(1, { message: "必須項目です" });
+
+export const postFormSchema = z.object({
+  title: required(),
+  body: required(),
+});
+
+export type PostFormSchemaType = z.infer<typeof postFormSchema>;
