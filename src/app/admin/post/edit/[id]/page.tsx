d8ea46e49d13b336f77981255ca661af89028aaa
Koji Uegaki <kjugkjk@gmail.com>
2025-03-21 11:53:21 +0900

    update Next to v15

--- src/app/admin/post/edit/[id]/page.tsx
+++ src/app/admin/post/edit/[id]/page.tsx
@@ -2,7 +2,8 @@ import { EditPostContainer } from "@/features/post/components/editPostContainer/
 import { getPost } from "@/features/post/serverFunctions/getPost";
 import { notFound } from "next/navigation";
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   const post = await getPost(params.id);
 
   if (post === null) {
