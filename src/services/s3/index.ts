aab139a459f27496646591358cf7033ffa2db476
Koji Uegaki <kjugkjk@gmail.com>
2025-03-30 12:47:54 +0900

    feat: setup minio

--- src/services/s3/index.ts
+++ src/services/s3/index.ts
@@ -8,5 +8,6 @@ export const createS3Client = () => {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
     },
+    endpoint: process.env.AWS_ENDPOINT || "",
   });
 };
