579557256548a60d4f0c81a4ab7fccea32dcf820
Koji Uegaki <kjugkjk@gmail.com>
2025-04-19 14:20:46 +0900

    migration

---
+++ prisma/migrations/20250419050446_add_ogp_image_field/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "ogImageFilename" TEXT;
+
+-- AlterTable
+ALTER TABLE "_PostToTag" ADD CONSTRAINT "_PostToTag_AB_pkey" PRIMARY KEY ("A", "B");
+
+-- DropIndex
+DROP INDEX "_PostToTag_AB_unique";
