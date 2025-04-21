36bba96d33eaeb4991b22a75326e10ab95269257
Koji Uegaki <kjugkjk@gmail.com>
2025-04-21 17:13:14 +0900

    feat: marked に message-box 拡張を追加

--- src/services/markdown/index.ts
+++ src/services/markdown/index.ts
@@ -1,4 +1,4 @@
-import { Marked, Tokens } from "marked";
+import { Marked, Tokens, MarkedExtension } from "marked";
 import { markedHighlight } from "marked-highlight";
 
 import hljs from "highlight.js/lib/core";
@@ -41,6 +41,36 @@ const renderer: any = {
   },
 };
 
+// カスタムメッセージボックス拡張
+const messageBoxExtension: MarkedExtension = {
+  extensions: [
+    {
+      name: "message-box",
+      level: "block",
+      tokenizer(src: string, tokens: any) {
+        const rule = /^:::message\s*\n([\s\S]*?)\n:::\s*(?:\n|$)/;
+        const match = rule.exec(src);
+        if (match) {
+          const token = {
+            type: "message-box",
+            raw: match[0],
+            text: match[1].trim(),
+            tokens: this.lexer.inlineTokens(match[1].trim(), []),
+          };
+          return token;
+        }
+        return undefined;
+      },
+      renderer(token: any) {
+        if (!token || !token.tokens) {
+          return "";
+        }
+        return `<div class="bg-red-200 rounded p-2">${this.parser.parseInline(token.tokens)}</div>\n`;
+      },
+    },
+  ],
+};
+
 export function getMarkedInstance() {
   if (!instance) {
     const _instance = new Marked(
@@ -55,6 +85,7 @@ export function getMarkedInstance() {
     );
 
     _instance.use({ breaks: true, renderer }); // eslint-disable-line
+    _instance.use(messageBoxExtension);
 
     instance = _instance;
   }
