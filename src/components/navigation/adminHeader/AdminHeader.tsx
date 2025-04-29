303986481ffc572c2854e621ffade44acf1efd6f
Koji Uegaki <kjugkjk@gmail.com>
2025-04-29 10:52:06 +0900

    fix: header link

--- src/components/navigation/adminHeader/AdminHeader.tsx
+++ src/components/navigation/adminHeader/AdminHeader.tsx
@@ -8,7 +8,7 @@ export const AdminHeader = () => {
   return (
     <Container as="header" size="xl" className="px-8">
       <div className="pt-8 pb-6 flex items-center gap-12">
-        <Link href="/admin/posts/draft">
+        <Link href="/posts">
           <h1 className={`${bokor.className} text-3xl`}>My Next Blog</h1>
         </Link>
         <nav className="flex-1">
