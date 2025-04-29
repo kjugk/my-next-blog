a28a0a70f5654e007939fc7b38fb19239e0e7732
Koji Uegaki <kjugkjk@gmail.com>
2025-04-29 11:11:45 +0900

    fix: edit post page

--- src/features/post/components/editor/HtmlPreview.tsx
+++ src/features/post/components/editor/HtmlPreview.tsx
@@ -8,7 +8,7 @@ export const HtmlPreview = ({ body }: Props) => {
   const marked = getMarkedInstance();
 
   return (
-    <div className="mt-5 p-4 border rounded prose dark:prose-invert max-w-full bg-base-100 overflow-y-auto">
+    <div className="px-4 border rounded prose dark:prose-invert max-w-full bg-transparent">
       <div dangerouslySetInnerHTML={{ __html: marked.parse(body) }} />
     </div>
   );
